d22af681420193f7f35dfa4eb83b8bd0a5e1d5d2
masterUNG <c.it.rbac@gmail.com>
2017-08-05 22:37:53 +0700

    home 22.37 5aug2560

---
+++ app/src/main/java/rbac/nilchart/chu/chu1/fragment/MainFragment.java
@@ -0,0 +1,20 @@
+package rbac.nilchart.chu.chu1.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 5/8/2560.
+ */
+
+public class MainFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main)
+    }
+}
